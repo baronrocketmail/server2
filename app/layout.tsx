e4d92111d3557d57d6657bcf885a7bf62e73b4e1
sammy freddie <sammyfreddie@unknownf4d4887bc1bf.attlocal.net>
2022-11-03 09:16:20 -0700

    initial

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,12 @@
-import localFont from '@next/font/local';
-import './globals.css'
 
-
-const myFont = localFont({ src: './GoldmanSans_W_Th.woff2' });
-
-export default function RootLayout({ children }: {
-  children: React.ReactNode;
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
 }) {
-    return (
-            <html lang="en" className={myFont.className}>
-                <head>
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                </head>
-                <body>{children}</body>
-            </html>
-            );
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
 }
